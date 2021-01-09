8a242d1c67fd773f84358718284800a171802d46
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-09 12:41:59 +0100

    import images and add to array

--- src/components/Memory.js
+++ src/components/Memory.js
@@ -1,12 +1,29 @@
 import { useState } from 'react';
 
+import Game from './Game'
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../images', false, /\.(png|jpe?g|svg)$/));
+
+var ary = []
+var i = 0;
+
+for (var key in images) {
+  ary.push({id: i, link: images[key].default})
+  i++
+}
 
 function Memory() {
-  const [ array, setArray ] = useState([]);
+  const [ array, setArray ] = useState(ary);
 
   return ( 
     <div>
-      <h1>Hello</h1>
+      <img  src={array[6].link} />
     </div>
   )
 }
